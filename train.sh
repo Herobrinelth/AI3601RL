5c511aa521eb4a187ce57dc06154a4b0671d05fc
Herobrinelth <109146676+Herobrinelth@users.noreply.github.com>
2024-06-02 15:58:01 +0800

    state

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --batch_size 128 --hidden_size 1024 --learning_rate 1e-3 --bc_weight 0.1
+CUDA_VISIBLE_DEVICES=1 python train.py --batch_size 128 --hidden_size 1024 --learning_rate 1e-3 --bc_weight 0
